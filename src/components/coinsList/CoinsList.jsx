f25872135817ceb7a3816616cbfd7a2f7e0cce97
Manuel <manuel_8ale@hotmail.com>
2021-07-20 21:18:34 -0400

    refactor, from static data to redux data

--- src/components/coinsList/CoinsList.jsx
+++ src/components/coinsList/CoinsList.jsx
@@ -1,68 +1,11 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { List } from "./styles";
 import { DefaultContainer } from "../layout/DefaultContainer";
 import LoadingSpinner from "../loadingSpinner/LoadingSpinner";
 import CoinsListItem from "./CoinsListItem";
-
-const data = [
-  {
-    name: "Bitcoin",
-    abbreviation: "BTC",
-    price: "$55,749.37",
-    percentage: +12.53,
-    img: "./coins-icons/svg/btc.svg",
-  },
-  {
-    name: "Ethereum",
-    abbreviation: "ETH",
-    price: "$3,393.12",
-    percentage: +10.12,
-    img: "./coins-icons/svg/eth.svg",
-  },
-  {
-    name: "Cardano",
-    abbreviation: "ADA",
-    price: "$1.756",
-    percentage: +7.23,
-    img: "./coins-icons/svg/ada.svg",
-  },
-  {
-    name: "XRP",
-    abbreviation: "XRP",
-    price: "$1.536",
-    percentage: +3.26,
-    img: "./coins-icons/svg/xrp.svg",
-  },
-  {
-    name: "Dogecoin",
-    abbreviation: "DOGE",
-    price: "$0.421",
-    percentage: +33.53,
-    img: "./coins-icons/svg/doge.svg",
-  },
-  {
-    name: "Bitcoin Cash",
-    abbreviation: "BCH",
-    price: "$10.756.23",
-    percentage: +1.23,
-    img: "./coins-icons/svg/bch.svg",
-  },
-
-  {
-    name: "BAT",
-    abbreviation: "BAT",
-    price: "$1.12",
-    percentage: +22.98,
-    img: "./coins-icons/svg/bat.svg",
-  },
-  {
-    name: "Stellar Lumens",
-    abbreviation: "XLM",
-    price: "$0.604",
-    percentage: -11.22,
-    img: "./coins-icons/svg/xlm.svg",
-  },
-];
+import { useSelector } from "react-redux";
+import { ratesSelector } from "../../redux/rates/ratesSlice";
+import { balancesSelector } from "../../redux/balance/balanceSlice";
 
 const compareLowerCase = (string1, string2) => {
   return string1.toLowerCase().includes(string2.toLowerCase());
@@ -72,27 +15,28 @@ const compareUpperCase = (string1, string2) => {
   return string1.includes(string2.toUpperCase());
 };
 
-const filterCoins = (searchParameter) => {
+const filterCoins = (searchParameter, data) => {
   return data.filter(
     (coin) =>
       compareLowerCase(coin.name, searchParameter) ||
-      compareUpperCase(coin.abbreviation, searchParameter)
+      compareUpperCase(coin.currency, searchParameter)
   );
 };
 
-const asyncData = (searchParameter) => {
-  const data = filterCoins(searchParameter);
-  console.log(data);
+const fakeAsync = (searchParameter, data) => {
+  const result = filterCoins(searchParameter, data);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (!data || data.length === 0) reject("Error");
-      resolve(data);
+      if (!result) reject("Error");
+      resolve(result);
     }, 2000);
   });
 };
 
 export default function CoinsList({ searchParameter }) {
-  const [coinsData, setCoinsData] = useState([]);
+  const rates = useSelector(ratesSelector);
+  const balances = useSelector(balancesSelector);
+  const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
@@ -101,8 +45,8 @@ export default function CoinsList({ searchParameter }) {
       try {
         setLoading(true);
         setError(false);
-        let response = await asyncData(searchParameter);
-        setCoinsData(response);
+        let response = await fakeAsync(searchParameter, balances);
+        setResults(response);
         setLoading(false);
       } catch (error) {
         setError(true);
@@ -116,15 +60,22 @@ export default function CoinsList({ searchParameter }) {
 
   return (
     <DefaultContainer>
-      {coinsData.length !== 0 && !loading && (
+      {results.length !== 0 && !loading && (
         <List>
-          {coinsData.map((coin, index) => (
-            <CoinsListItem key={index} coin={coin} />
+          {results.map((coin, index) => (
+            <CoinsListItem
+              key={index}
+              coin={{ ...coin, ...rates[coin.currency] }}
+            />
           ))}
         </List>
       )}
       {loading && <LoadingSpinner />}
       {error && "An error ocurred"}
+      {searchParameter !== "" &&
+        results.length === 0 &&
+        !loading &&
+        "No results"}
     </DefaultContainer>
   );
 }
