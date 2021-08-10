7b13b286fbc56a81a5bd562f959c4a7a8e511bbf
Manuel <manuel_8ale@hotmail.com>
2021-08-10 07:08:03 -0400

    add svgs illustrations

--- src/components/coinsList/CoinsList.jsx
+++ src/components/coinsList/CoinsList.jsx
@@ -4,8 +4,11 @@ import { DefaultContainer } from "../layout/DefaultContainer";
 import LoadingSpinner from "../loadingSpinner/LoadingSpinner";
 import CoinsListItem from "./CoinsListItem";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 import { ratesSelector } from "../../redux/rates/ratesSlice";
 import coins from "../../coins-abbreviations.json";
+import { ReactComponent as Searching } from "../../icons/searching.svg";
+import { ReactComponent as NotFound } from "../../icons/not_found.svg";
 
 const compareLowerCase = (string1, string2) => {
   return string1.toLowerCase().includes(string2.toLowerCase());
@@ -31,10 +34,32 @@ const fakeAsync = (searchParameter, data) => {
   });
 };
 
+const SvgContainer = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  @media only screen and (min-width: 768px) {
+    width: 50%;
+  }
+  & > svg {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const SpinnerContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const Container = styled(DefaultContainer)`
+  min-height: 27rem;
+  position: relative;
+`;
+
 export default function CoinsList({ searchParameter }) {
   const rates = useSelector(ratesSelector);
-  console.log(coins);
-  //const balances = useSelector(balancesSelector);
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -58,7 +83,7 @@ export default function CoinsList({ searchParameter }) {
   }, [searchParameter]);
 
   return (
-    <DefaultContainer>
+    <Container>
       {results.length !== 0 && !loading && (
         <List>
           {results.map((coin, index) => (
@@ -69,12 +94,28 @@ export default function CoinsList({ searchParameter }) {
           ))}
         </List>
       )}
-      {loading && <LoadingSpinner />}
+      {searchParameter === "" && !loading && (
+        <>
+          <h1>Start searching now!</h1>
+          <SvgContainer>
+            <Searching />
+          </SvgContainer>
+        </>
+      )}
+      {loading && (
+        <SpinnerContainer>
+          <LoadingSpinner />
+        </SpinnerContainer>
+      )}
       {error && "An error ocurred"}
-      {searchParameter !== "" &&
-        results.length === 0 &&
-        !loading &&
-        "No results"}
-    </DefaultContainer>
+      {searchParameter !== "" && results.length === 0 && !loading && (
+        <>
+          <h1>We couldn't find what you're looking for</h1>
+          <SvgContainer>
+            <NotFound />
+          </SvgContainer>
+        </>
+      )}
+    </Container>
   );
 }
