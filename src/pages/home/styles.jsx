47e859b8ec98ed04b63aece82bd80c534f2d75b3
Manuel <manuel_8ale@hotmail.com>
2021-08-13 10:30:17 -0400

    delete unused file

--- src/pages/home/styles.jsx
+++
@@ -1,3 +0,0 @@
-import styled from "styled-components";
-import { MainContainer } from "../../components/layout/FlexLayout";
-import { DefaultContainer } from "../../components/layout/DefaultContainer";
